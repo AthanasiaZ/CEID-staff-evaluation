85d06af50fe356a7eb4a91dac0c986efcc0d6597
itsikelis <tsikelis.i@protonmail.com>
2021-08-05 16:23:36 +0300

    Added create table commands for managers and employees (also employee languages).

--- CREATE DATABASE IF NOT EXISTS staffEval.sql
+++ CREATE DATABASE IF NOT EXISTS staffEval.sql
@@ -24,5 +24,35 @@ CREATE TABLE IF NOT EXISTS users
     PRIMARY KEY(username)
 )
 
+CREATE TABLE IF NOT EXISTS managers
+(
+    user_username CHAR,
+    company CHAR,
+    experience INT,
+    PRIMARY KEY(user_username),
+    CONSTRAINT USRMNGR FOREIGN KEY (user_username) REFERENCES users(username)
+    ON DELETE CASCADE ON UPDATE CASCADE
+)
 
+CREATE TABLE IF NOT EXISTS employees
+(
+    user_username CHAR,
+    company_ID_number INT,
+    company_AFM INT,
+    experience INT,
+    resume CHAR,
+    certifications CHAR,
+    praises CHAR, 
+    PRIMARY KEY(user_username),
+    CONSTRAINT USRMPL FOREIGN KEY (user_username) REFERENCES users(username)
+    ON DELETE CASCADE ON UPDATE CASCADE
+)
+
+CREATE TABLE IF NOT EXISTS languages
+(
+    employee_username CHAR,
+    language CHAR,
+    CONSTRAINT LANG FOREIGN KEY (employee_username) REFERENCES emploeyee(user_username)
+    ON DELETE CASCADE ON UPDATE CASCADE
+)
 
